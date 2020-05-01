1dbe7c23eabfe231c76e07c6edb1d8d622010f1c
Fuenzalida Lorca, Branko <branko.fuenzalida@wolterskluwer.com>
2020-05-01 08:21:21 +0200

    Removed unnecessary android errors.

--- capacitor/plugins-library/android/src/main/java/com/krikoo/capacitor/models/SharerError.java
+++ capacitor/plugins-library/android/src/main/java/com/krikoo/capacitor/models/SharerError.java
@@ -3,8 +3,8 @@ package com.krikoo.capacitor.models;
 public class SharerError {
   public static String EmptyData = "EMPTY_DATA_ERROR";
   public static String EmptyFilePath = "EMPTY_FILE_PATH_ERROR";
-  public static String AppNotFound = "APP_NOT_FOUND";
-  public static String UnsupportedUrl = "UNSUPPORTED_URL";
+  public static String AppNotFound = "APP_NOT_FOUND_ERROR";
+  public static String UnsupportedUrl = "UNSUPPORTED_URL_ERROR";
   public static String FileConversion = "FILE_CONVERSION_ERROR";
   public static String FileToUri = "FILE_TO_URI_ERROR";
   public static String InvalidFileParam = "INVALID_FILE_PARAM_ERROR";
